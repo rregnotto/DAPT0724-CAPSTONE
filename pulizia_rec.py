72e7023d7b1a073c8b09715d6a084b0191801a00
rregnotto <francesca.regnotto1@gmail.com>
2025-06-02 10:34:11 +0200

    pulizia rec

--- pulizia_rec.py
+++ pulizia_rec.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import os
+import numpy as np
 
 path = "/Users/francescaregnotto/Workspace/DAPT0724/CAPSTONE/"
 file = os.path.join(path, "reclami.xlsx")
@@ -26,9 +27,30 @@ df.columns = ["ID", "Codice", "Prodotto", "Data_prod", "Mix", "X", "Stabilimento
                "Stamp", "Data_stamp", "X", "X", "Provincia", "Regione", "Posa", "Area", 
                "X", "X", "Linea_Gronda", "X", "X", "slm", "Coordinate", "X", "X", "X", "X", 
                "X", "X", "X"]
-
+print(df.head())
 #Rimuovo le colonne non necessarie
 df = df.drop(columns = ["X"])
 print(df.columns)
 
-#Rimuovo 
+#Rimuovo gli spazi all'inizio e alla fine delle stringhe
+df = df.apply(lambda x: x.str.strip() if x.dtype == "object" else x)
+
+#Sostituisco i valori nulli con None
+df = df.replace(["0"], np.nan)
+print(df["Mix"]) #check se i valori sono stati sostituiti correttamente
+
+#Sistemo i valori nella colonna Mix
+df["Mix"] = df["Mix"].str.replace("/", "")
+df["Mix"] = df["Mix"].str.replace(" ", "", regex=False)
+
+print(df["Mix"])
+
+#Sostituisco gli spazi interni con _
+df = df.apply(lambda x: x.str.replace(" ", "_", regex=False) if x.dtype == "object" else x)
+
+#Converto in formato date le colonne con data
+
+date_col = ["Data_prod", "Data_Fatt", "Data_rec"]
+
+for col in date_col:
+    df[col] = pd.to_datetime(df[col], format= "%d/%m/%Y", errors ='coerce')
